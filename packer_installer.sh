8bdf0a9625145a5fb54237b7d299a31a6cdd2271
phanikumar <phanikumarp@users.noreply.github.com>
2021-03-15 10:25:55 +0530

    Update packer_installer.sh

--- packer_installer.sh
+++ packer_installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "*********Downloading *********"
-sudo apt-get update 
+##sudo apt-get update 
 gsutil cp gs://opsmx-restapp-debs/restapp_1.0_all.deb .
 sudo dpkg -i restapp_1.0_all.deb
 echo "restapp started...."
